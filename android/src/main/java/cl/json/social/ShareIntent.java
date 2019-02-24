ba78a1928580ba726f0fca1da7c6003290bfb127
sansantosh <san.shrestha@gmail.com>
2019-02-23 23:07:06 -0800

    mail to support

--- android/src/main/java/cl/json/social/ShareIntent.java
+++ android/src/main/java/cl/json/social/ShareIntent.java
@@ -41,7 +41,9 @@ public abstract class ShareIntent {
         if (ShareIntent.hasValidKey("subject", options) ) {
             this.getIntent().putExtra(Intent.EXTRA_SUBJECT, options.getString("subject"));
         }
-
+        if (ShareIntent.hasValidKey("email", options) ) {
+            this.getIntent().putExtra(Intent.EXTRA_EMAIL, new String[] { options.getString("email") });
+        }
         if (ShareIntent.hasValidKey("title", options) ) {
             this.chooserTitle = options.getString("title");
         }
